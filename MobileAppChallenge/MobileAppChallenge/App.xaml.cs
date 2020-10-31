09aa6e1385d144630a765a33517f4bb10019b4bb
its-AliRaza <its.aliraza03@gmail.com>
2020-10-31 11:20:26 +0500

    Fonts, Styles and Icons are added

--- MobileAppChallenge/MobileAppChallenge/App.xaml.cs
+++ MobileAppChallenge/MobileAppChallenge/App.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using MobileAppChallenge.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Roboto-Bold.ttf", Alias = "RobotoBold")]
+[assembly: ExportFont("Roboto-Light.ttf", Alias = "RobotoLight")]
+[assembly: ExportFont("Roboto-Medium.ttf", Alias = "RobotoMedium")]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias = "RobotoRegular")]
 namespace MobileAppChallenge
 {
     public partial class App : Application
@@ -9,8 +14,7 @@ namespace MobileAppChallenge
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new StartPage();
         }
 
         protected override void OnStart()
